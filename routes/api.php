cb00d20b79e821bf825214757e50e1e426396ef8
Putra Prima Arhandi <putraprima@gmail.com>
2022-01-16 06:50:52 +0700

    Fixing Auth Using Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\QuoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::apiResource('quote', QuoteController::class);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('quote', QuoteController::class);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+});
